a389a4ba2cc49e3924a3363df677376bdfbb30ed
Avallah <kamelia.daudel@gmail.com>
2018-04-30 17:17:04 +0100

    Modified readData.py - able to differentiate the data if diff = true
    Added signature.py
    - able to calculate the signature for several paths

--- readData.py
+++ readData.py
@@ -1,13 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-def readData(pathToCsvFile):
+#We implement our own reader instead of using panda, because panda failed for files that are too big 
+def readData(pathToCsvFile, diff = True): #diff: preprocessing of the data by differenciation
     out = []
-    i = 0
     with open(pathToCsvFile) as f:
         f.readline()
         for line in f:
             line = line.replace('"', '').strip().split(',')
-            out.append([line[0],[float(x) for x in line[1:] if x != '']])
+            if diff != True:
+                out.append([line[0],np.array([float(x) for x in line[1:] if x != '']).reshape((-1,1))]) #need to reshape for the signature
+            else: 
+                tmp = np.diff([float(x) for x in line[1:] if x != ''])
+                out.append([line[0],np.array(tmp[1:-1]).reshape((-1,1)), 1 if tmp[-1] >= 0 else 0])
+        print("file " + pathToCsvFile + " read")
     return out
 
+#Read the info file with panda
+def readInfoData(pathToCsvFile):
+    out = pd.read_csv(pathToCsvFile)
+    print("file " + pathToCsvFile + " read")
+    return out
---
+++ signature.py
@@ -0,0 +1,14 @@
+import numpy as np
+import esig.tosig as ts
+import sklearn
+
+def calculateSignature(path,order):
+    time = np.array(range(len(path[1]))).reshape((-1,1))
+    two_dim_stream = np.append(path[1],time, axis=1)
+    out = ts.stream2sig(two_dim_stream, 2)[1:]
+    return out
+
+def calculateMultipleSignatures(listpath,order):
+    signatures = [calculateSignature(listpath[i],2) for i in range(len(listpath))]
+    print("Signature calculated at order " + str(order) +" for all paths")
+    return signatures
